72de35295765d3cc1d5e11e11991c19f9886ab44
Arnaud Garnier <arnaudgarnier06@gmail.com>
2017-01-16 18:04:37 +0100

    Add chat room

--- Server/app.js
+++ Server/app.js
@@ -5,11 +5,15 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 var accessEnigmas = require('./routes/accessEnigmas');
 
-var app = express();
+var app = express()
+    ,http = require('http')
+    ,server = http.createServer(app)
+    io = require('socket.io').listen(server);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,18 +26,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
   next();
 });
-
-
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
@@ -44,11 +42,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
 app.use('/', index);
 app.use('/users', users);
 app.use('/access', accessEnigmas);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -57,9 +53,51 @@ app.use(function(req, res, next) {
 });
 
 var PORT = 8888;
-
-var listener = app.listen(PORT, function(){
+var listener = server.listen(PORT, function(){
   console.log('Listening on port ' + listener.address().port); //Listening on port 8888
+});
+
+// usernames which are currently connected to the chat
+var usernames = {};
+
+// rooms which are currently available in chat
+var rooms = ['room'];
+
+io.sockets.on('connection', function (socket) {
+
+    // when the client emits 'adduser', this listens and executes
+    socket.on('adduser', function(username){
+        // store the username in the socket session for this client
+        socket.username = username;
+        // store the room name in the socket session for this client
+        socket.room = 'room';
+        // add the client's username to the global list
+        usernames[username] = username;
+        // send client to the room
+        socket.join('room');
+        // echo to client they've connected
+        socket.emit('updatechat', 'SERVER', 'you have connected to the chat');
+        // echo to room that a person has connected to the room
+        socket.broadcast.to('room').emit('updatechat', 'SERVER', username + ' has connected to the chat');
+        socket.emit('updateusernames', usernames, 'username');
+    });
+
+    // when the client emits 'sendchat', this listens and executes
+    socket.on('sendchat', function (data) {
+        // we tell the client to execute 'updatechat' with 2 parameters
+        io.sockets.in(socket.room).emit('updatechat', socket.username, data);
+    });
+
+    // when the user disconnects.. perform this
+    socket.on('disconnect', function(){
+        // remove the username from global usernames list
+        delete usernames[socket.username];
+        // update list of users in chat, client-side
+        io.sockets.emit('updateusers', usernames);
+        // echo globally that this client has left
+        socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+        socket.leave(socket.room);
+    });
 });
 
 module.exports = app;
