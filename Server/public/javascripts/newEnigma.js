7858ecc2bf8fd9659e05fd99d970bc5234153674
Chloé GUGLIELMI <guglielmi.chloe@etu.unice.fr>
2017-02-09 09:25:50 +0100

    Choisir la position sur une carte

--- Server/public/javascripts/newEnigma.js
+++ Server/public/javascripts/newEnigma.js
@@ -1,7 +1,10 @@
+
 var xhr = new XMLHttpRequest;
 
 var url = 'https://web-map-project-si5.herokuapp.com';
 //var url = 'http://localhost:8888';
+
+
 
 var post = function () {
 
@@ -16,28 +19,17 @@ var post = function () {
         "contenu" : ""
     };
 
-    var newPos = function(position){
-        enigma.titre = document.getElementById("titre").value;
-        enigma.coo.lat = position.coords.latitude;
-        enigma.coo.lng = position.coords.longitude;
-        enigma.point = parseInt(document.getElementById("points").value);
-        enigma.contenu = document.getElementById("contenu").value;
-        console.log(JSON.stringify(enigma));
-        xhr.open("POST", url + "/enigmas/new", true);
-        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-        xhr.send(JSON.stringify(enigma));
-        alert("Enigme envoyée");
-    };
+    enigma.titre = document.getElementById("titre").value;
+    enigma.coo.lat = document.getElementById("lat").value;
+    enigma.coo.lng = document.getElementById("lng").value;
+    enigma.point = parseInt(document.getElementById("points").value);
+    enigma.contenu = document.getElementById("contenu").value;
+    console.log(JSON.stringify(enigma));
+    xhr.open("POST", url + "/enigmas/new", true);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.send(JSON.stringify(enigma));
+    alert("Enigme envoyée");
 
-    var error = function (error) {
-        console.log("navigator doesn't support geolocation");
-    };
-
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(newPos,error);
-    } else{
-        console.log("Geolocation is not supported by this browser.");
-    }
 };
 
 
