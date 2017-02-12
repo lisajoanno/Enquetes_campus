5105642fb7137bf802756af4edda74ae0ccaa91c
lisajoanno <lisa.joanno@gmail.com>
2017-02-12 20:47:25 +0100

    more tests

--- Server/test/test.js
+++ Server/test/test.js
@@ -43,6 +43,45 @@ describe('TEAM', function () {
                 expect(res.body).to.not.be.null;
                 var a = (ObjectID(res.body));
                 expect(ObjectID.isValid(a)).to.be.true;
+
+                return chai.request(app)
+                    .get('/team/all')
+                    .then(function (res) {
+                        expect(res).to.have.status(200);
+                    });
+
             });
     });
 });
+
+describe('ENIGMAS', function () {
+
+    it('POST of a new enigma', function () {
+        return chai.request(app)
+            .post('/enigmas/new')
+            .send({
+                titre : "un titre",
+                coo : {
+                    lat : 43,
+                    lng : 7
+                },
+                point : 20,
+                contenu : "l'énigme",
+                image : "img.png"
+            })
+            .then(function (res) {
+                expect(res).to.have.status(200);
+                expect(res.body).to.not.be.null;
+            });
+    });
+
+    it('GET of all enigmas', function () {
+        return chai.request(app)
+            .get('/enigmas')
+            .then(function (res) {
+                expect(res).to.have.status(200);
+                expect(res.body).to.be.empty;
+            });
+    });
+});
+
